bdd420f6fc7fc0a53e6ebe5ad4d1149b985eae58
Faizuddin Mohammed <faiz@konic.net>
2025-07-22 07:07:19 +0530

    feat: pass image base64 as input

--- image.py
+++ image.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from dotenv import load_dotenv
@@ -7,28 +8,31 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-history = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "input_text",
-                "text": "What do you think is the brand of the car in the image?",
-            },
-            {
-                "type": "input_image",
-                "image_url": "https://upload.wikimedia.org/wikipedia/commons/8/8e/Bugatti_Typ_13_Brescia_Sport-Racing_1922.jpg",
-            },
-        ],
-    }
-]
-while True:
-    prompt = input("User: ")
-    history.append({"role": "user", "content": prompt})
-    response = client.responses.create(
-        model="gpt-4o",
-        input=history,
-    )
-    history.append({"role": "assistant", "content": response.output_text})
+# image -> base64 -> text
 
-    print(response.output_text)
+image_path = "images/book.jpg"
+
+with open(image_path, "rb") as image:
+    image_base64 = base64.b64encode(image.read()).decode("utf-8")
+
+print(image_base64)
+response = client.responses.create(
+    model="gpt-4o",
+    input=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "input_text",
+                    "text": "What is this book about?",
+                },
+                {
+                    "type": "input_image",
+                    "image_url": f"data:image/jpeg;base64,{image_base64}",
+                },
+            ],
+        }
+    ],
+)
+
+print(response.output_text)
